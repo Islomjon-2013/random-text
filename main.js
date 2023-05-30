22a0728f8d0d3912f6b1cf26c1cc7040f935a0a8
hulkar <hulkar@gmail.com>
2023-05-30 19:44:53 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,146 @@
+const text = [
+  `
+  New York
+  City
+  Midtown Manhattan
+  Manhattan, looking southward from Top of the Rock
+  Central Park scenery
+  Central Park
+  The Unisphere, a large metal globe sculpture
+  Unisphere, Queens
+  Brooklyn Bridge
+  Brooklyn Bridge
+  Grand Central Terminal
+  Grand Central Terminal
+  Statue of Liberty
+  Statue of Liberty
+  United Nations headquarters building, with flags in foreground
+  United Nations headquarters
+  Bright lights of Times Square
+  Times Square
+  Entrance to the Bronx Zoo
+  Bronx Zoo
+  Staten Island Ferry boat on the water
+  Staten Island Ferry
+  Flag of New York
+  Flag
+  Official seal of New York
+  Seal
+  Official logo of New York
+  Wordmark
+  Nicknames: The Big Apple, The City That Never Sleeps, Gotham,[1] and others
+  MapWikimedia | © OpenStreetMap
+  Show New York City
+  Show New York
+  Show the United States
+  Show all
+  Coordinates: 40°42′46″N 74°00′22″W[2]
+  Country	United States
+  State	New York
+  Region	Mid-Atlantic
+  Constituent counties (boroughs)	Bronx (The Bronx)
+  Kings (Brooklyn)
+  New York (Manhattan)
+  Queens (Queens)
+  Richmond (Staten Island)
+  Historic colonies	New Netherland
+  Province of New York
+  Settled	1624
+  Consolidated	1898
+  Named for	James, Duke of York
+  Government
+   • Type	Strong mayor–council
+   • Body	New York City Council
+   • Mayor	Eric Adams (D)
+  Area[3]
+   • Total	472.43 sq mi (1,223.59 km2)
+   • Land	300.46 sq mi (778.18 km2)
+   • Water	171.97 sq mi (445.41 km2)
+  Elevation[4]	33 ft (10 m)
+  Population (2020)[5]
+   • Total	8,804,190
+   • Estimate (July 2021)[6]	8,467,513
+   • Rank	1st in the United States
+  1st in New York State
+   • Density	29,302.66/sq mi (11,313.81/km2)
+   • Urban[7]	19,426,449
+   • Urban density	5,980.8/sq mi (2,309.2/km2)
+   • Metro[8]	20,140,470
+  Demonym	New Yorker
+  Time zone	UTC−05:00 (EST)
+   • Summer (DST)	UTC−04:00 (EDT)
+  ZIP Codes	
+  100xx–104xx, 11004–05, 111xx–114xx, 116xx
+  Area code(s)	212/646/332, 718/347/929, 917
+  FIPS code	36-51000
+  GNIS feature ID	975772
+  GDP (City, 2021)	$886 billion[9] (1st)
+  GMP (Metro, 2021)	$2.0 trillion[citation needed] (1st)
+  Largest borough by area	Queens (109 sq mi or 280 km2)
+  Largest borough by population	Brooklyn (2020 Census 2,736,074)
+  Largest borough by GDP (2021)	Manhattan ($651.6 billion)[9]
+  Website	nyc.gov Edit this at Wikidata
+  Part of a series on
+  Regions of New York
+  Map of New York Economic Regions.svg
+  Downstate New York
+  New York CityLong IslandHudson Valley (Lower)
+  Upstate New York
+  Hudson Valley (Middle and Upper)Capital DistrictNorth CountrySouthern TierMohawk ValleyCentral New YorkFinger LakesWestern New York
+  Administrative divisions
+  Timelines of town creation
+  vte
+  New York, often called New York City[a] or NYC, is the most populous city in the United States. With a 2020 population of 8,804,190 distributed over 300.46 square miles (778.2 km2), New York City is the most densely populated major city in the United States and more than twice as populous as Los Angeles, the nation's second-largest city. The city also has a population that is larger than that of 38 individual U.S. states. New York City is located at the southern tip of New York State. The city constitutes the geographical and demographic center of both the Northeast megalopolis and the New York metropolitan area, the largest metropolitan area in the U.S. by both population and urban area. With over 20.1 million people in its metropolitan statistical area and 23.5 million in its combined statistical area as of 2020, New York is one of the world's most populous megacities, and over 58 million people live within 250 mi (400 km) of the city.[10] New York City is a global cultural, financial, high-tech, entertainment, glamor,[11] and media center with a significant influence on commerce, health care and life sciences,[12] research, technology, education, politics, tourism, dining, art, fashion, and sports. Home to the headquarters of the United Nations, New York is an important center for international diplomacy,[13][14] and is sometimes described as the capital of the world.
+   `,
+  `
+  Situated on one of the world's largest natural harbors and extending into the Atlantic Ocean, New York City comprises five boroughs, each of which is coextensive with a respective county of the state of New York. The five boroughs, which were created in 1898 when local governments were consolidated into a single municipal entity, are: Brooklyn (in Kings County), Queens (in Queens County), Manhattan (in New York County), The Bronx (in Bronx County), and Staten Island (in Richmond County)
+             
+   `,
+  `As of 2021, the New York metropolitan area is the largest metropolitan economy in the world with a gross metropolitan product of over $2.4 trillion. If the New York metropolitan area were a sovereign state, it would have the eighth-largest economy in the world. New York City is an established safe haven for global investors.[18] New York is home to the highest number of billionaires,[19][20] individuals of ultra-high net worth (greater than US$30 million),[21] and millionaires of any city in the world.[22]
+   `,
+  `The city and its metropolitan area constitute the premier gateway for legal immigration to the United States. As many as 800 languages are spoken in New York,[23] making it the most linguistically diverse city in the world. New York City is home to more than 3.2 million residents born outside the U.S., the largest foreign-born population of any city in the world as of 2016.[24]
+
+
+   `,
+  `
+  New York City traces its origins to a trading post founded on the southern tip of Manhattan Island by Dutch colonists in approximately 1624. The settlement was named New Amsterdam (Dutch: Nieuw Amsterdam) in 1626 and was chartered as a city in 1653. The city came under British control in 1664 and was renamed New York after King Charles II of England granted the lands to his brother, the Duke of York.[25][26] The city was regained by the Dutch in July 1673 and was renamed New Orange for one year and three months; the city has been continuously named New York since November 1674. New York City was the capital of the United States from 1785 until 1790,[27] and has been the largest U.S. city since 1790. The Statue of Liberty greeted millions of immigrants as they came to the U.S. by ship in the late 19th and early 20th centuries, and is a symbol of the U.S. and its ideals of liberty and peace.[28] In the 21st century, New York City has emerged as a global node of creativity, entrepreneurship,[29] and as a symbol of freedom and cultural diversity.[30] The New York Times has won the most Pulitzer Prizes for journalism and remains the U.S. media's "newspaper of record".[31] In 2019, New York City was voted the greatest city in the world in a survey of over 30,000 people from 48 cities worldwide, citing its cultural diversity.[32]
+`,
+  `
+
+Many districts and monuments in New York City are major landmarks, including three of the world's ten most visited tourist attractions in 2023.[33] A record 66.6 million tourists visited New York City in 2019. Times Square is the brightly illuminated hub of the Broadway Theater District,[34] one of the world's busiest pedestrian intersections[35] and a major center of the world's entertainment industry.[36] Many of the city's landmarks, skyscrapers, and parks are known around the world, and the city's fast pace led to the phrase New York minute. The Empire State Building is a global standard of reference to describe the height and length of other structures.[37]
+
+
+`,
+  `
+
+New York's real estate market is the most expensive in the world.[38] Providing continuous 24/7 service and contributing to the nickname The City That Never Sleeps, the New York City Subway is the largest single-operator rapid transit system in the world with 472 passenger rail stations, and Penn Station in Midtown Manhattan is the busiest transportation hub in the Western Hemisphere.[39] The city features over 120 colleges and universities, including some of the world's top universities.[40] Its public urban university system, the City University of New York, is the largest in the nation.[41] Anchored by Wall Street in the Financial District of Lower Manhattan, New York City has been called both the world's leading financial and fintech center[42] and the most economically powerful city in the world,[43] and is home to the world's two largest stock exchanges by total market capitalization, the New York Stock Exchange and Nasdaq.[44][45]
+`,
+  `
+  The Stonewall Inn in Greenwich Village, part of the Stonewall National Monument, is considered the historic epicenter of LGBTQ+ culture[46] and the birthplace of the modern gay rights movement.[47][48] New York City is the headquarters of the global art market, with numerous art galleries and auction houses collectively hosting half of the world's art auctions; and the Metropolitan Museum of Art is both the largest and most visited art museum in the United States and hosts the globally focused Met Gala haute couture fashion event annually.[49] Governors Island in New York Harbor is planned to host a US$1 billion research and education center as a leader in the climate crisis.[50]
+`,
+  `
+  Early history
+Main article: History of New York City (prehistory–1664)
+In the pre-Columbian era, the area of present-day New York City was inhabited by Algonquian Native Americans, including the Lenape. Their homeland, known as Lenapehoking, included the present-day areas of Staten Island, Manhattan, the Bronx, the western portion of Long Island (including the areas that would later become the boroughs of Brooklyn and Queens), and the Lower Hudson Valley.[53]
+`,
+];
+let form = document.querySelector(".lorem-form");
+let amouunt = document.getElementById("amount");
+let result = document.querySelector(".lorem-text");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const value = parseInt(amouunt.value);
+  const random = Math.floor(Math.random() * text.length);
+  if (isNaN(value) || value <= 0 || value > 9) {
+    result.innerHTML = `<p class="result">${text[random]}</p>`;
+  } else {
+    let textTemp = text.slice(0, value);
+    textTemp
+      .map(function (item) {
+        return;
+        `<p class="item">${item}</p>`;
+      })
+      .join("");
+    result.innerHTML = textTemp;
+  }
+});
